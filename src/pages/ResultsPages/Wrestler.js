21d33809ebb92ff2a63d855b19c225b8e15b0a6f
Bryan Martinez <bryanmartinez2401@gmail.com>
2023-01-30 17:19:13 -0500

    minor changes, beginning to test wrestler query

--- src/pages/ResultsPages/Wrestler.js
+++ src/pages/ResultsPages/Wrestler.js
@@ -15,23 +15,37 @@ Wrestler Page will display
 */
 
 import React, { useState } from "react";
-import "react-dropdown/style.css";
 import "./wrestler.css";
+import Parse from "parse/dist/parse.min.js";
 import { useLocation, useNavigate } from "react-router-dom";
 
 export default function Wrestler() {
-  // Sort Dropdown Content
-  const sortOptions = [
-    "Default",
-    "A-Z",
-    "Z-A",
-    "Age (Rising)",
-    "Age (Falling)",
-  ];
-  const defaultOption = sortOptions[0];
+  const [search, setSearch] = useState("");
+  const [sort, setSort] = useState("Default");
 
-  const [search, setSearch] = useState("Default");
-  const [sort, setSort] = useState(defaultOption);
+  // When User hits enter in searchbar,search hook is updated
+  // and query is also updated
+  const newSearch = (event) => {
+    if (event.key === "Enter") {
+      console.log(
+        "User Searched: " + document.getElementById("searchInput").value
+      );
+      setSearch(document.getElementById("searchInput").value);
+      startQuery();
+    }
+  };
+
+  // When User changes sort dropdown
+  // and query will be updated
+  function changeSort(value) {
+    if (value === 0) setSort("Default");
+    if (value === 1) setSort("A-Z");
+    if (value === 2) setSort("Z-A");
+    if (value === 3) setSort("Age (Rising)");
+    if (value === 4) setSort("Age (Falling)");
+    startQuery();
+  }
+
   // Dropdown Functions
   // Proper Implemnation to be Added
   // Functions to Open and Close Dropdown
@@ -55,30 +69,6 @@ export default function Wrestler() {
     }
   }
 
-  // When User hits enter in searchbar,search hook is updated
-  // and query is also updated
-  const newSearch = (event) => {
-    if (event.key === "Enter") {
-      console.log("Enter key pressed");
-      console.log(
-        "User Searched: " + document.getElementById("searchInput").value
-      );
-      setSearch(document.getElementById("searchInput").value);
-      startQuery();
-    }
-  };
-
-  // When User changes sort dropdown
-  // and query will be updated
-  function changeSort(value) {
-    if (value === 0) setSort("Default");
-    if (value === 1) setSort("A-Z");
-    if (value === 2) setSort("Z-A");
-    if (value === 3) setSort("Age (Rising)");
-    if (value === 4) setSort("Age (Falling)");
-    startQuery();
-  }
-
   // Function for when Filters Option is added
   function updateFilters() {
     // To be Added
@@ -86,10 +76,19 @@ export default function Wrestler() {
   }
 
   // Query is Started
-  function startQuery() {
+  async function startQuery() {
     console.log("Search: " + search);
     console.log("Sort: " + sort);
-    alert("onload function works");
+    let wrestlerObject = new Parse.object.extend("Wrestler");
+    try {
+      const wrestlerQuery = new Parse.Query(wrestlerObject);
+      const wrestlerResults = await wrestlerQuery.find();
+      console.log(wrestlerResults);
+      alert("onload function works");
+    } catch (error) {
+      alert(`Error! ${error.message}`);
+      return false;
+    }
   }
 
   let display = (
@@ -100,7 +99,7 @@ export default function Wrestler() {
   );
 
   return (
-    <div className="body" onLoad={startQuery()}>
+    <div className="body">
       <div className="searchbar">
         <div className="bar">
           <img className="searchIcon" src="/search_icon.png"></img>
