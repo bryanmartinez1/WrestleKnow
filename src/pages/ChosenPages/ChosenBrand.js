533c99b2f4533cc4b897a113688bb615f8c674e9
Bryan <bryanmartinez2401@gmail.com>
2023-02-16 01:18:34 -0500

    minor changes

--- src/pages/ChosenPages/ChosenBrand.js
+++ src/pages/ChosenPages/ChosenBrand.js
@@ -19,5 +19,6 @@ import { useLocation } from "react-router-dom";
 export default function ChosenBrand() {
   const location = useLocation();
   const id = location.state.id;
+
   return <h1>Brand</h1>;
 }
